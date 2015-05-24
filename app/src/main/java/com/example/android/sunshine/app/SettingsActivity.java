5f0dad17eb0ba31942a10b36f7e9a70a1d625e1d
Dan Galpin <dgalpin@google.com>
2015-05-24 15:04:31 -0700

    Updated settings to show the status of the typed-in location.

--- app/src/main/java/com/example/android/sunshine/app/SettingsActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/SettingsActivity.java
@@ -96,29 +96,28 @@ public class SettingsActivity extends PreferenceActivity
             if (prefIndex >= 0) {
                 preference.setSummary(listPreference.getEntries()[prefIndex]);
             }
-        } else if (key.equals(getString(R.string.pref_location_key))){
-            // If we changed the location, the summary should include the validity of location
+        } else if (key.equals(getString(R.string.pref_location_key))) {
             @SunshineSyncAdapter.LocationStatus int status = Utility.getLocationStatus(this);
-            switch(status){
+            switch (status) {
                 case SunshineSyncAdapter.LOCATION_STATUS_OK:
                     preference.setSummary(stringValue);
                     break;
                 case SunshineSyncAdapter.LOCATION_STATUS_UNKNOWN:
-                    preference.setSummary(getString(
-                            R.string.pref_location_unknown_description, value));
+                    preference.setSummary(getString(R.string.pref_location_unknown_description, value.toString()));
                     break;
                 case SunshineSyncAdapter.LOCATION_STATUS_INVALID:
-                    preference.setSummary(getString(
-                            R.string.pref_location_error_description, value));
+                    preference.setSummary(getString(R.string.pref_location_error_description, value.toString()));
                     break;
                 default:
+                    // Note --- if the server is down we still assume the value
+                    // is valid
                     preference.setSummary(stringValue);
             }
-
         } else {
             // For other preferences, set the summary to the value's simple string representation.
             preference.setSummary(stringValue);
         }
+
     }
 
     // This gets called before the preference is changed
@@ -142,15 +141,10 @@ public class SettingsActivity extends PreferenceActivity
         } else if ( key.equals(getString(R.string.pref_units_key)) ) {
             // units have changed. update lists of weather entries accordingly
             getContentResolver().notifyChange(WeatherContract.WeatherEntry.CONTENT_URI, null);
-
         } else if ( key.equals(getString(R.string.pref_location_status_key)) ) {
             // our location status has changed.  Update the summary accordingly
             Preference locationPreference = findPreference(getString(R.string.pref_location_key));
             bindPreferenceSummaryToValue(locationPreference);
-
-        } else if ( key.equals(getString(R.string.pref_icons_key))){
-            // changed our icon pack, trigger a re-sync
-            SunshineSyncAdapter.syncImmediately(this);
         }
     }
 
